7d273d8788eec260aeab817558c6d348e229cca7
amarouane <amarouane@itsc.uah.edu>
2020-01-31 09:42:08 -0600

    Filtring subnets to not use the one requiring the outbound internet

--- aws-data-sources-tf/main.tf
+++ aws-data-sources-tf/main.tf
@@ -6,6 +6,10 @@ data "aws_vpc" "application_vpc" {
 
 data "aws_subnet_ids" "subnet_ids" {
   vpc_id = data.aws_vpc.application_vpc.id
+   filter {
+    name   = "tag:Name"
+    values = ["${var.subnet_tag_name} *"]
+  }
 }
 
 data "aws_lambda_function" "sts_credentials" {
--- aws-data-sources-tf/variables.tf
+++ aws-data-sources-tf/variables.tf
@@ -5,3 +5,7 @@ variable "sts_credentials_function_name" {
 variable "vpc_tag_name" {
     default = "Application VPC"
 }
+
+variable "subnet_tag_name" {
+    default = "Private application"
+}
