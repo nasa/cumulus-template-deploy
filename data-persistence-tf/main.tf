a3750bba61e9480f83400d6540abf2868eee6285
Marc Huffnagle <marc@element84.com>
2020-02-21 12:10:59 -0500

    Pass tags into modules

--- data-persistence-tf/main.tf
+++ data-persistence-tf/main.tf
@@ -1,11 +1,3 @@
-module "data_persistence" {
-  source = "https://github.com/nasa/cumulus/releases/download/v1.19.0/terraform-aws-cumulus.zip//tf-modules/data-persistence"
-
-  prefix                     = var.prefix
-  subnet_ids                 = var.subnet_ids
-  include_elasticsearch      = var.include_elasticsearch
-}
-
 variable "prefix" {
   type = string
 }
@@ -28,6 +20,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+module "data_persistence" {
+  source = "https://github.com/nasa/cumulus/releases/download/v1.19.0/terraform-aws-cumulus.zip//tf-modules/data-persistence"
+
+  prefix                     = var.prefix
+  subnet_ids                 = var.subnet_ids
+  include_elasticsearch      = var.include_elasticsearch
+
+  tags = {
+    Deployment = var.prefix
+  }
+}
+
 output "dynamo_tables" {
   value = module.data_persistence.dynamo_tables
 }
@@ -47,4 +51,3 @@ output "elasticsearch_security_group_id" {
 output "elasticsearch_alarms" {
   value = module.data_persistence.elasticsearch_alarms
 }
-
