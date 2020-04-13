2626b35e67e0f7ea1f21c6e080b7a3b33b2d846b
Michael Brandt <michaelbrandt5@gmail.com>
2020-04-13 14:45:31 -0600

    update URLs to use v1.21.0

--- cumulus-tf/hello_world_workflow.tf
+++ cumulus-tf/hello_world_workflow.tf
@@ -1,5 +1,5 @@
 module "hello_world_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v1.20.0/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v1.21.0/terraform-aws-cumulus-workflow.zip"
 
   prefix                                = var.prefix
   name                                  = "HelloWorldWorkflow"
