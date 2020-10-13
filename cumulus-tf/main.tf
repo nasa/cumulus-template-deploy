820e9ef906658bd0f4150e93377499cdc319cb30
Mark Boyd <markdavidboyd@gmail.com>
2020-10-13 14:48:38 -0400

    update data-persistence deployment (#51)
    * update data-persistence deployment
    
    * update release artifact version numbers
    
    * update release artifacts

--- cumulus-tf/main.tf
+++ cumulus-tf/main.tf
@@ -9,7 +9,7 @@ locals {
 }
 
 module "cumulus" {
-  source = "https://github.com/nasa/cumulus/releases/download/v1.20.0/terraform-aws-cumulus.zip//tf-modules/cumulus"
+  source = "https://github.com/nasa/cumulus/releases/download/v3.0.0/terraform-aws-cumulus.zip//tf-modules/cumulus"
 
   cumulus_message_adapter_lambda_layer_arn = var.cumulus_message_adapter_lambda_layer_arn
 
