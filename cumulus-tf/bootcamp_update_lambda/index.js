532d38c383ae535677a23729009c70d9a0b02591
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-11-19 05:40:15 -0700

    Add example lambda

---
+++ cumulus-tf/bootcamp_update_lambda/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { runCumulusTask } = require('@cumulus/cumulus-message-adapter-js');
+const log = require('@cumulus/common/log');
+
+/**
+ * Return payload object: {
+ *   input: payload from previous lambda,
+ *   buckets: buckets object from `config` CMA object
+ * }
+ *
+ * @param {Object} event - input from the message adapter
+ * @returns {Object} sample JSON object
+ */
+async function bootcampExample(event) {
+  return {
+    buckets: event.config.buckets,
+    input: event.input,
+  };
+}
+/**
+ * Lambda handler
+ *
+ * @param {Object} event      - a Cumulus Message
+ * @param {Object} context    - an AWS Lambda context
+ * @returns {Promise<Object>} - sample JSON object
+ */
+async function handler(event, context) {
+  return runCumulusTask(bootcampExample, event, context);
+}
+
+exports.handler = handler;
