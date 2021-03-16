de47a35e79058d448a03d0405a006f0a35dbd441
jennyhliu <34660846+jennyhliu@users.noreply.github.com>
2021-03-16 14:58:11 -0400

    Upgrade to cumulus v7.0.0 (#59)
    * Upgrade to cumulus v7.0.0

--- cumulus-tf/hello_world_workflow.tf
+++ cumulus-tf/hello_world_workflow.tf
@@ -1,5 +1,5 @@
 module "hello_world_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v5.0.0/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v7.0.0/terraform-aws-cumulus-workflow.zip"
 
   prefix          = var.prefix
   name            = "HelloWorldWorkflow"
