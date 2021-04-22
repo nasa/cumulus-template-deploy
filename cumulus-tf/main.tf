f844ecb207917954c49d977aee502f960248b904
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-04-22 11:48:21 -0600

    Update template to use 8.0.0 refs

--- cumulus-tf/main.tf
+++ cumulus-tf/main.tf
@@ -41,7 +41,7 @@ data "terraform_remote_state" "data_persistence" {
 }
 
 module "cumulus" {
-  source = "https://github.com/nasa/cumulus/releases/download/v7.0.0/terraform-aws-cumulus.zip//tf-modules/cumulus"
+  source = "https://github.com/nasa/cumulus/releases/download/v8.0.0/terraform-aws-cumulus.zip//tf-modules/cumulus"
 
   cumulus_message_adapter_lambda_layer_version_arn = var.cumulus_message_adapter_lambda_layer_version_arn
 
