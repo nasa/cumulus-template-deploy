4b80e69c6098663291d7b4f316316e5585450aa6
Jennifer Tran <tranjennif@gmail.com>
2021-04-30 08:51:06 -0700

    Fix typo

--- data-migration1-tf/main.tf
+++ data-migration1-tf/main.tf
@@ -33,7 +33,7 @@ module "data_migration1" {
   dynamo_tables = data.terraform_remote_state.data_persistence.outputs.dynamo_tables
 
   rds_security_group_id = data.terraform_remote_state.data_persistence.outputs.rds_security_group_id
-  rds_user_access_secret_arn = data.terraform_remote_state.data_persistence.rds_user_access_secret_arn
+  rds_user_access_secret_arn = data.terraform_remote_state.data_persistence.outputs.rds_user_access_secret_arn
   rds_connection_heartbeat = var.rds_connection_heartbeat
 
   provider_kms_key_id = var.provider_kms_key_id
