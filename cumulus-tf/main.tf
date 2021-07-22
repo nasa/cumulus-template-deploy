25711f9ab00ad890dea646212627f5a9f2bbd342
Nate Pauzenga <npauzenga@gmail.com>
2021-07-22 14:15:19 -0400

    Update main.tf

--- cumulus-tf/main.tf
+++ cumulus-tf/main.tf
@@ -57,6 +57,7 @@ module "cumulus" {
   vpc_id            = var.vpc_id
   lambda_subnet_ids = var.lambda_subnet_ids
 
+  ecs_cluster_instance_image_id   = var.ecs_cluster_instance_image_id
   ecs_cluster_instance_subnet_ids = length(var.ecs_cluster_instance_subnet_ids) == 0 ? var.lambda_subnet_ids : var.ecs_cluster_instance_subnet_ids
   ecs_cluster_min_size            = 1
   ecs_cluster_desired_size        = 1
