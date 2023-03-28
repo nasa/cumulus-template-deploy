e9117b2ae1f9e98d253c655f981958883d56c8e0
Vanh <90708967+vpnguye2@users.noreply.github.com>
2023-03-28 10:22:38 -0500

    Update Log retention config CUMULUS-3121 for data-migration1-tf

--- data-migration1-tf/variables.tf
+++ data-migration1-tf/variables.tf
@@ -38,3 +38,16 @@ variable "vpc_id" {
   default = null
 }
 
+variable "cloudwatch_log_retention_periods" {
+  type = map(number)
+  description = "retention periods for the respective cloudwatch log group, these values will be used instead of default retention days"
+  default = {
+    data-migration-1 = 10, #data-migration1 module
+  }
+}
+
+variable "default_log_retention_days" {
+  type = number
+  default = 15
+  description = "default value that user chooses for their log retention periods"
+}
