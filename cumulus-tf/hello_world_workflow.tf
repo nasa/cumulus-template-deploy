f3fa2f01f6069e6394a3c893d9201a30a34ff387
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2023-05-30 15:57:32 -0400

    Update version to 15.0.2

--- cumulus-tf/hello_world_workflow.tf
+++ cumulus-tf/hello_world_workflow.tf
@@ -1,5 +1,5 @@
 module "hello_world_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v11.1.1/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v15.0.2/terraform-aws-cumulus-workflow.zip"
 
   prefix          = var.prefix
   name            = "HelloWorldWorkflow"
