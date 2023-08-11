4b0af46d183027a34f08eeb84fc0b64abb8ba0ef
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2023-08-11 06:16:27 -0600

    Update to v16.1.1

--- data-persistence-tf/main.tf
+++ data-persistence-tf/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 data "aws_region" "current" {}
 
 module "data_persistence" {
-  source = "https://github.com/nasa/cumulus/releases/download/v15.0.2/terraform-aws-cumulus.zip//tf-modules/data-persistence"
+  source = "https://github.com/nasa/cumulus/releases/download/v16.1.1/terraform-aws-cumulus.zip//tf-modules/data-persistence"
 
   prefix                         = var.prefix
   subnet_ids                     = var.subnet_ids
