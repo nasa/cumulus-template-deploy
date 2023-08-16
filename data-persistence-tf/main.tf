f167ac84869270dd697fc3d0ff333ea451749fce
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2023-08-16 17:26:42 -0400

    Fixup

--- data-persistence-tf/main.tf
+++ data-persistence-tf/main.tf
@@ -19,7 +19,7 @@ data "aws_region" "current" {}
 
 module "data_persistence" {
   source = "https://github.com/nasa/cumulus/releases/download/v17.0.0/terraform-aws-cumulus.zip//tf-modules/data-persistence"
-  
+
   prefix                         = var.prefix
   subnet_ids                     = var.subnet_ids
   include_elasticsearch          = var.include_elasticsearch
