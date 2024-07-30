08174eb878f8093025dbe1fddda3b1833ef47819
Paul Pilone <paul@pilone.org>
2024-07-30 10:04:45 -0400

    Updates Terraform resources to use Cumulus release 18.3.2 (#89)

--- data-persistence-tf/main.tf
+++ data-persistence-tf/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 data "aws_region" "current" {}
 
 module "data_persistence" {
-  source = "https://github.com/nasa/cumulus/releases/download/v18.3.1/terraform-aws-cumulus.zip//tf-modules/data-persistence"
+  source = "https://github.com/nasa/cumulus/releases/download/v18.3.2/terraform-aws-cumulus.zip//tf-modules/data-persistence"
 
   prefix                         = var.prefix
   subnet_ids                     = var.subnet_ids
