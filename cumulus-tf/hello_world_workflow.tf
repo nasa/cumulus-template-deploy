bc4416f3a02435ae841e6186b5f4addce9b73a35
jennyhliu <jenny.h.liu@nasa.gov>
2025-08-07 14:53:20 -0400

    release-20.2.0

--- cumulus-tf/hello_world_workflow.tf
+++ cumulus-tf/hello_world_workflow.tf
@@ -1,5 +1,5 @@
 module "hello_world_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v18.4.0/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v20.2.0/terraform-aws-cumulus-workflow.zip"
 
   prefix          = var.prefix
   name            = "HelloWorldWorkflow"
