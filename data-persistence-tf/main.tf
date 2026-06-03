1d45fd4e0364e0de6d90d637400168171c40c27e
Naga Nages <66387215+Nnaga1@users.noreply.github.com>
2026-06-03 14:50:23 -0400

    release PR 21.3.5 (#103)

--- data-persistence-tf/main.tf
+++ data-persistence-tf/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 data "aws_region" "current" {}
 
 module "data_persistence" {
-  source = "https://github.com/nasa/cumulus/releases/download/v21.3.4/terraform-aws-cumulus.zip//tf-modules/data-persistence"
+  source = "https://github.com/nasa/cumulus/releases/download/v21.3.5/terraform-aws-cumulus.zip//tf-modules/data-persistence"
 
   prefix                         = var.prefix
   subnet_ids                     = var.subnet_ids
